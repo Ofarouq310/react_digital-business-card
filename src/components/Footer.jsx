eb8f05084f7d3f41e8b24ecc178a4594bfff70e6
Omar Farouq <ofarouq310@gmail.com>
2023-01-08 17:10:14 +0200

    Add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export default function Footer(){
+    return(
+        <div className='footer'>
+            <img className='footer-img' src='./src/assets/Twitter Icon.png' alt='twitter-icon' />
+            <img className='footer-img' src='./src/assets/Facebook Icon.png' alt='facebook-icon' />
+            <img className='footer-img' src='./src/assets/Linkedin Icon.png' alt='linkedin-icon' />
+            <img className='footer-img' src='./src/assets/GitHub Icon.png' alt='github-icon' />
+        </div>
+    )
+}
