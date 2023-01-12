cad6d515f958c0e315820de760ac040fae15dd61
Omar Farouq <ofarouq310@gmail.com>
2023-01-12 16:34:41 +0200

    Update components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,21 @@ import React from 'react'
 export default function Footer(){
     return(
         <div className='footer'>
-            <img className='footer-img' src='./src/assets/Twitter Icon.png' alt='twitter-icon' />
-            <img className='footer-img' src='./src/assets/Facebook Icon.png' alt='facebook-icon' />
-            <img className='footer-img' src='./src/assets/Linkedin Icon.png' alt='linkedin-icon' />
-            <img className='footer-img' src='./src/assets/GitHub Icon.png' alt='github-icon' />
+            <a href="https://www.twitter.com/Ofarouq310" target={'_blank'}>
+                <img className='footer-img' src='./src/assets/Twitter Icon.png' alt='twitter-icon' />
+            </a>
+
+            <a href="https://www.facebook.com/Ofarouq310" target={'_blank'}>
+                <img className='footer-img' src='./src/assets/Facebook Icon.png' alt='facebook-icon' />
+            </a>
+
+            <a href="https://www.linkedin.com/in/Ofarouq310/" target={'_blank'}>    
+                <img className='footer-img' src='./src/assets/Linkedin Icon.png' alt='linkedin-icon' />
+            </a>
+
+            <a href="https://www.github.com/Ofarouq310" target={'_blank'}>
+                <img className='footer-img' src='./src/assets/GitHub Icon.png' alt='github-icon' />
+            </a>
         </div>
     )
 }
